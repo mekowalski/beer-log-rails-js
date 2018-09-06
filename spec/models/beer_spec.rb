ad212d52013ac06845373ba8b15548b7103bc1a6
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-06 11:03:19 -0600

    modify beer_spec test, test shows properly, migrate db

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 describe Beer do
+  it 'can be created' do
+    beer = Beer.create!(name: 'Backside Stout', description: 'this stout is freaking delicious', abv: '6.20',
+    location: 'Durango, CO')
+    expect(beer).to be_valid
+  end
 
 end
