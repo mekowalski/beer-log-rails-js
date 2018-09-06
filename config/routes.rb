975222f10bc25dcc09867336f1b8971e73280d38
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-06 12:08:54 -0600

    modify routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/about' => 'beers#about'
-  get '/beers' => 'beers#index'
-  get '/beers/:id' => 'beers#show'
-  
+
+  resources :beers, only: [:index, :show]
+
 end
