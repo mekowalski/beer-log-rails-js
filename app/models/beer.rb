8ec25420ed09e93ea88392d1f2868a6f023c7437
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-06 11:37:47 -0600

    create beer_summary test and passed with creating beer_summary def

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,7 @@
 class Beer < ActiveRecord::Base
 
+  def beer_summary
+    self.name + ' - ' + self.description
+  end
+
 end
--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe Beer do
   it 'can be created' do
-    beer = Beer.create!(name: 'Backside Stout', description: 'this stout is freaking delicious', abv: '6.20',
+    beer = Beer.create!(name: 'Backside Stout', description: 'This stout is freaking delicious', abv: '6.20',
     location: 'Durango, CO')
     expect(beer).to be_valid
+    expect(beer.beer_summary).to eq('Backside Stout - This stout is freaking delicious')
   end
 
 end
