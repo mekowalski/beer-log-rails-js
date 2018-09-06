3ed65a52b646cd5fb33d05ea44db958a763aede3
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-06 13:16:46 -0600

    remove new beer spec, tests were not working correctly, i will just have a long spec file

--- spec/features/beer_spec.rb
+++ spec/features/beer_spec.rb
@@ -30,4 +30,33 @@ describe 'navigate' do
     end
   end
 
+  describe 'new beer' do
+    it "ensures that the form route works with the /new action" do
+      visit new_beer_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it "renders HTML in the /new template" do
+      visit new_beer_path
+      expect(page).to have_content('Add a New Beer')
+    end
+
+    it "displays a new beer form that redirects to the index page, which then contains the submitted beer's
+    name, description, abv and location" do
+      visit new_beer_path
+      fill_in 'beer_name', with: 'My newest beer'
+      fill_in 'beer_description', with: 'Newest beer description'
+      fill_in 'beer_abv', with: 'Newest beer ABV'
+      fill_in 'beer_location', with: 'Newest Beer Location'
+
+      click_on 'Submit Beer'
+
+      expect(path.current_path).to eq(beers_path)
+      expect(page).to have_content('My newest beer')
+      expect(page).to have_content('Newest beer description')
+      expect(page).to have_content('Newest beer ABV')
+      expect(page).to have_content('Newest Beer Location')
+    end
+  end
+
 end
