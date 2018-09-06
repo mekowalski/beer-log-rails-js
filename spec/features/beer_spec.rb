d1085bb806a0d0e41c172d970c716136c8818405
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-06 13:05:58 -0600

    form test, add h3 to new file

--- spec/features/beer_spec.rb
+++ spec/features/beer_spec.rb
@@ -33,7 +33,12 @@ describe 'navigate' do
   describe 'new beer' do
     it "ensures that the form route works with the /new action" do
       visit new_beer_path
-      expect(page.status_code).to eq(200)    
+      expect(page.status_code).to eq(200)
+    end
+
+    it "renders HTML in the /new template" do
+      visit new_beer_path
+      expect(page).to have_content('Add a New Beer')
     end
   end
 
