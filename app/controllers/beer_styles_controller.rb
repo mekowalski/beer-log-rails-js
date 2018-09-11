a9caa83506b0a38297734b6a4b94c98d958c515c
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-11 09:49:25 -0600

    testing a route in beerStyles con and adding to routes.rb, successful

--- app/controllers/beer_styles_controller.rb
+++ app/controllers/beer_styles_controller.rb
@@ -1,2 +1,7 @@
 class BeerStylesController < ApplicationController
+
+  def home
+    render :plain => 'Welcome to the Beer Styles page'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/about' => 'beers#about'
+  get '/home' => 'beer_styles#home'
 
   resources :beers, only: [:index, :show, :new, :create, :edit, :update]
   patch '/beers/:id', to: 'beer#update'
