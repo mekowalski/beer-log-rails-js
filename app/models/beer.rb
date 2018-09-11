52d923ffc284d235343e2dfc0b7ea281fa803d86
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-11 15:28:07 -0600

    comments regarding beer name uniqueness

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,5 +1,7 @@
 class Beer < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, uniqueness: true, presence: true
+  #because it's likely that no two beers will have the same name although, different breweries make produce
+  #a beer with the same name. I haven't encountered this
   validates :description, presence: true
   validates :abv, presence: true
   validates :location, presence: true
