48a25463ed21ddaeed8ee992ac3aa5065aa52f7f
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-11 14:12:02 -0600

    generate company resource, modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   get '/about' => 'beers#about'
+
+  resources :companies
 
   resources :beer_styles, only: [:index]
 
