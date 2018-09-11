9e12d4cfec2d10c9fa974faa18b09e2a598372c6
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-11 12:36:39 -0600

    remove comment and route in routes.rb, remove commemts from index view, link_to is clickable but not leading anywhere yet. may hold off for now

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/about' => 'beers#about'
-  get '/beer_styles_home' => 'beer_styles#beer_styles_home'
-  #i don't like the multiple words with routing & actions will make it better & nicer to look at eventually
-  #this does work successfully in browser
 
   resources :beer_styles, only: [:index]
 
