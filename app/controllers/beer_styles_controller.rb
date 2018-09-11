e35d804dbcbcf1123d39db938ba6e2fa895f25b8
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-11 11:31:21 -0600

    remove #beer_styles_home, comments for beer_styles index view

--- app/controllers/beer_styles_controller.rb
+++ app/controllers/beer_styles_controller.rb
@@ -1,10 +1,5 @@
 class BeerStylesController < ApplicationController
 
-  def beer_styles_home
-    render :plain => 'Welcome to the Beer Styles page'
-    #this is basically the index which is like the home page of Beer Styles. not needed
-  end
-
   def index
   end
 
