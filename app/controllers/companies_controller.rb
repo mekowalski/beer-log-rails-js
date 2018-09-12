cc1447dd3a6a4ab7d0bf327b036835484de465c1
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-12 16:30:15 -0600

    only built remaining CRUD actions, not yet tested

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,4 +8,36 @@ class CompaniesController < ApplicationController
     @company = Company.find(params[:id])
   end
 
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(params[:name])
+    if @company.save
+      redirect_to @company
+    else
+      render :new
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    @company.update(params[:name])
+    if @company.save
+      redirect_to @company
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @company = Company.find(params[:id])
+    @company.destroy
+    redirect_to companies_path
+  end
+
 end
