ae81217dc0d7d492dcf6dc0d8082ec05de4390f3
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-12 15:41:19 -0600

    notes for beerstyles associations

--- app/models/beer_style.rb
+++ app/models/beer_style.rb
@@ -1,5 +1,8 @@
 class BeerStyle < ApplicationRecord
   has_many :beers
+  #will beerstyles has_many :companies, through: :beers?
+  #could make sense: you can look up a beerstyle and see what companies brew this style of beer
+  #thinking cap, can come back to this if i need the association
 
   validates :name, presence: true
 end
