83dc58c16b6ef299bbbb31220d665e5b22da1c2f
Malind Kowalski <malind.nikulski@gmail.com>
2018-09-13 11:29:45 -0600

    create self method for beerstyle

--- app/models/beer_style.rb
+++ app/models/beer_style.rb
@@ -5,4 +5,10 @@ class BeerStyle < ApplicationRecord
   #thinking cap, can come back to this if i need the association
 
   validates :name, presence: true
+
+  #will beerstyle also have  a self method like Beer and Company
+  #i would assume so because when i create or update a beer with its attr, it will expect beerstyle relation
+  def BeerStyle
+    self.beer_style.name
+  end
 end
