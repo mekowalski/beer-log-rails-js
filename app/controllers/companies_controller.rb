8463ab886927c50993a9b0f78748413d424e7126
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-01 14:03:30 -0600

    commenting out all but beerstyle index and show actions

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,48 +1,49 @@
 class CompaniesController < ApplicationController
-  before_action :load_company, only: [:show, :edit, :update]
+  # before_action :load_company, only: [:show, :edit, :update]
 
   def index
     @companies = Company.all
   end
 
   def show
-  end
-
-  def new
-    @company = Company.new
-  end
-
-  def create
-    @company = Company.new(company_params)
-    if @company.save
-      redirect_to @company
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @company.update(company_params)
-      redirect_to @company
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    load_company.destroy
-    redirect_to companies_path
-  end
-
-  private
-  def load_company
     @company = Company.find(params[:id])
   end
 
-  def company_params
-    params.require(:company).permit([:name])
-  end
+  # def new
+  #   @company = Company.new
+  # end
+  #
+  # def create
+  #   @company = Company.new(company_params)
+  #   if @company.save
+  #     redirect_to @company
+  #   else
+  #     render :new
+  #   end
+  # end
+  #
+  # def edit
+  # end
+  #
+  # def update
+  #   if @company.update(company_params)
+  #     redirect_to @company
+  #   else
+  #     render :edit
+  #   end
+  # end
+  #
+  # def destroy
+  #   load_company.destroy
+  #   redirect_to companies_path
+  # end
+  #
+  # private
+  # def load_company
+  #   @company = Company.find(params[:id])
+  # end
+  #
+  # def company_params
+  #   params.require(:company).permit([:name])
+  # end
 end
