40de4d34d1786dcd15fa8c0f76b46a0a6d645e3e
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-02 13:13:07 -0600

    remove comments from beer model and beerstyle index

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -10,8 +10,3 @@ class Beer < ActiveRecord::Base
   accepts_nested_attributes_for :company, reject_if: :all_blank
   accepts_nested_attributes_for :beer_style, reject_if: :all_blank
 end
-
-# this is already a join table created kind of by accident but works well and makes sense to me
-# the beer already had the foreign keys of beer_style_id and company_id
-# one of the ways a lesson had created a join table was with a model combo of modelA_modelB join table
-# this didn't work with my app and example
