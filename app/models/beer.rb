c4c10a41374d0780a20a0e3b900f0cbced6aecf6
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-02 14:25:00 -0600

    more notes regarding nested forms

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -8,6 +8,8 @@ class Beer < ActiveRecord::Base
   validates :location, presence: true
 
   #provides nested forms
+  #not really a nested form but a very smart beer model
+  #(granted i'm using a macro and my resource is using a model class method)
   accepts_nested_attributes_for :company, reject_if: :all_blank
   accepts_nested_attributes_for :beer_style, reject_if: :all_blank
 end
