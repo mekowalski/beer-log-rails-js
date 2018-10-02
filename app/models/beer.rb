8ec0bc15b355877590166bc5bec2944004d84ce4
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-02 13:52:21 -0600

    comments for correct usage of nested forms

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -7,6 +7,7 @@ class Beer < ActiveRecord::Base
   validates :abv, presence: true
   validates :location, presence: true
 
+  #provides nested forms
   accepts_nested_attributes_for :company, reject_if: :all_blank
   accepts_nested_attributes_for :beer_style, reject_if: :all_blank
 end
