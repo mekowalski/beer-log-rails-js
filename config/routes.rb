06280acc766938b97d4aa95fcd09203aeb668dcc
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-09 14:51:41 -0600

    nesting beers in routes, minor route issue now

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   resources :companies
 
-  resources :beer_styles
-  get 'beerstyles/:id/beers', to: 'beerstyles#index'
-  get 'beerstyles/:id/beers/:id', to: 'beerstyles#show'
-
-  resources :beers
-  patch '/beers/:id', to: 'beer#update'
+  resources :beer_styles do
+    # nested resource for beers
+    resources :beers
+    patch '/beers/:id', to: 'beer#update'
+  end
+  # adding do...end to pass a block of its nested routes
+  # can still do to nested resources what's done with non-nested resources like limit to only certain actions
 
 end
