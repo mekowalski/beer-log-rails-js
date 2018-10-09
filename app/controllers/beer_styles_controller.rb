0c29b3624222852e87fd279d810767d6c8ad1327
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-09 14:16:17 -0600

    trying to create the proper actions, not seeing a change in nested routes

--- app/controllers/beer_styles_controller.rb
+++ app/controllers/beer_styles_controller.rb
@@ -1,11 +1,17 @@
 class BeerStylesController < ApplicationController
 
-  def index
-    @beer_styles = BeerStyle.all
+  # def index
+  #   @beer_styles = BeerStyle.all
+  # end
+
+  def beers_index
+    @beer_style = BeerStyle.find(params[:id])
+    @beers = BeerStyle.beers
+    render template: 'beer_styles/index'
   end
 
   def show
     @beer_style = BeerStyle.find(params[:id])
   end
-  
+
 end
