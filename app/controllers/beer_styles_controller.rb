6cd5bebcacd7fce17eb477df8c1f89240325e610
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-09 14:27:00 -0600

    create correct actions to display beers of a beerstyle but URL is incorrect

--- app/controllers/beer_styles_controller.rb
+++ app/controllers/beer_styles_controller.rb
@@ -10,8 +10,14 @@ class BeerStylesController < ApplicationController
     render template: 'beer_styles/index'
   end
 
-  def show
-    @beer_style = BeerStyle.find(params[:id])
-  end
+  # def show
+  #   @beer_style = BeerStyle.find(params[:id])
+  # end
 
+  def beer
+    @beer_style = BeerStyle.find(params[:id])
+    @beer = Beer.find(params[:beer_style_id])
+    render template: 'beers/show'
+  end
+  #this is the URL result, no bueno: http://localhost:3000/beers?utf8=%E2%9C%93&beer_style=3&commit=Filter
 end
