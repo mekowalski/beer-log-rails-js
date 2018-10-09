ccd41020a03890b66a7395296406ae2e9e186ab6
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-09 15:02:44 -0600

    fixed routing issues

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
 
   resources :beer_styles do
     # nested resource for beers
-    resources :beers
+    resources :beers, only: [:index, :show]
     patch '/beers/:id', to: 'beer#update'
   end
   # adding do...end to pass a block of its nested routes
   # can still do to nested resources what's done with non-nested resources like limit to only certain actions
 
+  resources :beers
 end
