fc017fa8814dbe112b95db022b98e78035c53efe
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-09 15:20:24 -0600

    notes regarding rails helpers

--- app/helpers/beer_styles_helper.rb
+++ app/helpers/beer_styles_helper.rb
@@ -1,2 +1,9 @@
 module BeerStylesHelper
+  #when i would use a helper
+  #doing calc in views
+  #more complex logic in views
+  #for display purposes
+  #eg: if i wanted to display a beer in a certain color dependent of the beer style
+    #eg: Lager = amber color
+    #    Stout = black color
 end
