43a0afac19073cf856d28e851b78ff380d01b3d7
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-15 13:30:39 -0600

    trying to redo/fix previous mistakes. currently running into a beerstyle id error in browser

--- app/controllers/beer_styles_controller.rb
+++ app/controllers/beer_styles_controller.rb
@@ -1,17 +1,17 @@
 class BeerStylesController < ApplicationController
 
-  # def index
-  #   @beer_styles = BeerStyle.all
-  # end
+  def index
+    @beer_styles = BeerStyle.all
+  end
+
+  def show
+    @beer_style = BeerStyle.find(params[:id])
+  end
 
   def beers_index
     @beer_style = BeerStyle.find(params[:id])
     @beers = BeerStyle.beers
     render template: 'beer_styles/index'
-  end
-
-  def show
-    @beer_style = BeerStyle.find(params[:id])
   end
 
   def beer
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   get '/home', to: 'beers#home' #this is kind of nice and i can use home buttom to get to index but not in love
 
-  resources :companies do
-    resources :beer_styles
-    resources :beers, only: [:index, :show]
-  end
+  resources :companies #do
+    # resources :beer_styles
+    # resources :beers, only: [:index, :show]
+  #end
 
   resource :beer_styles
+  # get 'beer_styles/:id/beers', to: 'beer_styles#beers_index'
+  # get 'beer_styles/:id/beers/:id', to: 'beer_styles#beer'
+
 
   resources :beers
   patch '/beers/:id', to: 'beer#update'
