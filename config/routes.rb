cd2669ab9b3b58692efee484c55b54500238044e
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-16 10:26:39 -0600

    trying to figure out nesting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   get '/home', to: 'beers#home' #this is kind of nice and i can use home buttom to get to index but not in love
 
-  resources :companies
+  resources :companies do
+    resources :beer_styles
+    resources :beers
+  end
   # get 'companies/:id/beer_styles'
   # get 'companies/:id/beer_styles/id'
   # in companies con i need to create the actions that would nest beerstyle index and show under company
 
   resources :beer_styles
+  get 'beer_styles/:id/beers', to: 'beer_styles#beers_index'
+  get 'beer_styles/:id/beers/:id', to: 'beer_styles#beer'
 
   resources :beers
   patch '/beers/:id', to: 'beer#update'
