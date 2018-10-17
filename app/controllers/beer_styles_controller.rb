d7b3c90b77f045f95bf043b922580b9e6365d289
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-17 17:09:19 -0600

    DRY BeerStyle controller

--- app/controllers/beer_styles_controller.rb
+++ app/controllers/beer_styles_controller.rb
@@ -1,23 +1,26 @@
 class BeerStylesController < ApplicationController
+  before_action :load_id, only: [:show, :beer, :beer_index]
 
   def index
     @beer_styles = BeerStyle.all
   end
 
   def show
-    @beer_style = BeerStyle.find(params[:id])
   end
 
   def beer
-    @beer_style = BeerStyle.find(params[:id])
     @beer = Beer.find(params[:beer_style_id])
     render template: 'beers/show'
   end
 
   def beer_index
-    @beer_style = BeerStyle.find(params[:id])
     @beers = @beer_style.beers
     render template: 'beers/index'
   end
   #this is the URL result, no bueno: http://localhost:3000/beers?utf8=%E2%9C%93&beer_style=3&commit=Filter
+
+  private
+  def load_id
+    @beer_style = BeerStyle.find(params[:id])
+  end
 end
