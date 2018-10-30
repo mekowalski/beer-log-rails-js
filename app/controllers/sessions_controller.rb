e4ff432b000f2a4f4d4e17e83493fe6d9f615a8b
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-30 13:15:40 -0600

    trying to implement login required

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   protect_from_forgery prepend:
+  # before_action :require_login
 
   def create
     session[:username] = params[:username]
@@ -10,4 +11,9 @@ class SessionsController < ApplicationController
     session.delete :username
     redirect_to '/home'
   end
+
+  # private
+  # def require_login
+  #   return head(:forbidden) unless session.include? :user_id
+  # end
 end
