72e533b1eb76b1e084623bbcbc1c7d573fe732d6
Malind Kowalski <malind.nikulski@gmail.com>
2018-10-30 12:31:32 -0600

    remove #new, play with logout options in application, beer notes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,6 @@
 class SessionsController < ApplicationController
   protect_from_forgery prepend:
 
-  def new
-  end
-
   def create
     session[:username] = params[:username]
     redirect_to '/beers'
