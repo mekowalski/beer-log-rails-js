ccdb3fcbbc53a3f1a33cbac4946fcdf94ed508a2
Malind Kowalski <malind.nikulski@gmail.com>
2018-11-05 13:02:01 -0700

    add prepend: true to appcon, comment current_user in applayout, placeholder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  
-  helper_method :current_user
+  protect_from_forgery with: :exception, prepend: true
+
+  # helper_method :current_user
 
   def home
     require_login
