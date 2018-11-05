74356c1dba42941f8a05e572f91f95a1e486483c
Malind Kowalski <malind.nikulski@gmail.com>
2018-11-05 15:52:59 -0700

    build sessions#create and notes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,13 @@ class SessionsController < ApplicationController
   # end
 
   def new
+  end
 
+  def create
+    user = User.find_by(params[:email])
+    # how does a user log in???
+    # HTTP is stateless protocol
+    # every request is independent of another
+    # you can't remember data in between requests
   end
 end
