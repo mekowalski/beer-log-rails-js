1c64efaad6130e770d73272ddf3f3056c1245e52
Malind Kowalski <malind.nikulski@gmail.com>
2018-11-05 13:35:00 -0700

    build #create, comment out all appcon actions, redirect fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,19 @@ class ApplicationController < ActionController::Base
 
   # helper_method :current_user
 
-  def home
-    require_login
-  end
-
-  def current_user
-    @current_user = (session[:username]) if session[:username]
-    # raise @current_user.inspect => nil
-    # raise session[:username].inspect => nil
-  end
-  # this is created but not sure if i'm writing this function correctly, it isn't working in the view
-
-  private
-  def require_login
-    redirect_to controller: 'sessions', action: 'create' unless current_user
-  end
+  # def home
+  #   require_login
+  # end
+  #
+  # def current_user
+  #   @current_user = (session[:username]) if session[:username]
+  #   # raise @current_user.inspect => nil
+  #   # raise session[:username].inspect => nil
+  # end
+  # # this is created but not sure if i'm writing this function correctly, it isn't working in the view
+  #
+  # private
+  # def require_login
+  #   redirect_to controller: 'sessions', action: 'create' unless current_user
+  # end
 end
