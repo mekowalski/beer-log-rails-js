ea60e9ae4c45fb9b97760def7fc1bb375065b703
Malind Kowalski <malind.nikulski@gmail.com>
2018-11-05 12:15:55 -0700

    create user model with email

--- AUTH.md
+++ AUTH.md
@@ -1,11 +1,11 @@
 # Step 1: Allow Users to register and login
 -How does this impact DB
-[] User model
+[x] User model
   -email
 
 -What URLs are needed
-[] GET /signup -form
-[] POST /signup -create the user
+[] GET /users/new -form
+[] POST /users -create the user
 
 [] GET /login -form
-[] POST /login -log the user in
+[] POST /sessions -log the user in
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_214730) do
+ActiveRecord::Schema.define(version: 2018_11_05_191307) do
 
   create_table "beer_styles", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2018_09_12_214730) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
