e73dc8634d25c3bb6913d0ca534cc424e27004c2
Malind Kowalski <malind.nikulski@gmail.com>
2018-11-05 16:38:02 -0700

    modify footer and build #current_user to display email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,11 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    session[:user_id]
+    !!current_user
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method #can call this in views with explicit direction of helper_method macro
 end
