cd05722550d820a43afe2b95d90b049c05f6a402
Malind Kowalski <malind.nikulski@gmail.com>
2018-12-11 13:21:20 -0700

    utilize jquery.ajax

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -1,10 +1,21 @@
 // playing in .js to practice Rails + AJAX
-//[]Hijack click event
+//[X]Hijack click event
 //[]Once in click event, fire AJAX request to get DATA
-//Need to place that DATA in the DOM, (add to page)
+//[]Need to place that DATA in the DOM, (add to page)
 
 $(function() {
   $('a.companies-beer').on('click', function(e) {
+    //Just clicked on this link
+    //use the HREF attr to as the URL
+    $.ajax({
+      method: 'GET',
+      url: this.href
+    })
+    .done(function(data) {
+      //GET the response
+      console.log(data)
+      //Want to load that data into the DOM
+    })
     e.preventDefault()
     alert('you clicked this link')
   })
