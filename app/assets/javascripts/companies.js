14ad52b2a2e74ef66f5b04db9890f235944910bf
Malind Kowalski <malind.nikulski@gmail.com>
2018-12-11 12:06:47 -0700

    praticing ajax, not working yet

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -1,3 +1,11 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+// playing in .js to practice Rails + AJAX
+
+$(function() { //this helps load the entire document for the click event to occur
+  $('a.companies').on('click', function(e) {
+    e.preventDefault()
+    alert('you clicked this link')
+  })
+})
