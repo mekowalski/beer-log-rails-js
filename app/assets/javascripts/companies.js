57aa7bf73488793cc975d0b79066afa9f7e9a323
Malind Kowalski <malind.nikulski@gmail.com>
2018-12-11 14:40:26 -0700

    rewrite ajax request to high-level abstraction

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -2,22 +2,29 @@
 //[X]Hijack click event
 //[X]Once in click event, fire AJAX request to get DATA
 //[X]Need to place that DATA in the DOM, (add to page)
+//[X]Rewrite AJAX in simpler way
 
 $(function() {
   $('a.companies-beer').on('click', function(e) {
     //Just clicked on this link
     //use the HREF attr to as the URL
-    $.ajax({
-      method: 'GET',
-      url: this.href
+
+    //rewrite high-level abstraction
+    $.get(this.href).success(function(response) {
+      $('div.beers').html(response)
     })
-    .done(function(response) { //calback function after AJAX(L10-13) is fired
-      //GET the response
-      $('div.beers').html(response) //this kind of works: it will sometimes go to a new page or
-      //sometimes will append to the DOM
-      //Want to load that data into the DOM
-    })
+
+    //low-level
+    // $.ajax({
+    //   method: 'GET',
+    //   url: this.href
+    // })
+    // .done(function(response) { //calback function after AJAX(L10-13) is fired
+    //   //GET the response
+    //   $('div.beers').html(response) //this kind of works: it will sometimes go to a new page or
+    //   //sometimes will append to the DOM
+    //   //Want to load that data into the DOM
+    // })
     e.preventDefault()
-    // alert('you clicked this link')
   })
 })
