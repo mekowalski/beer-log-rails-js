9d614b4ffb533a8539811c308669a4c09a450313
Malind Kowalski <malind.nikulski@gmail.com>
2018-12-11 13:18:19 -0700

    steps for AJAX

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -1,7 +1,7 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-// You can use CoffeeScript in this file: http://coffeescript.org/
 // playing in .js to practice Rails + AJAX
+//[]Hijack click event
+//[]Once in click event, fire AJAX request to get DATA
+//Need to place that DATA in the DOM, (add to page)
 
 $(function() {
   $('a.companies-beer').on('click', function(e) {
