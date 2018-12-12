fb1f2ed892118c6681f9567bb2e11e88de0f4ba3
Malind Kowalski <malind.nikulski@gmail.com>
2018-12-12 12:35:09 -0700

    correctly append response

--- app/assets/javascripts/beer_styles.js
+++ app/assets/javascripts/beer_styles.js
@@ -1,11 +1,11 @@
 //playing in .js to practice Rails + AJAX
-//[]Hijack click event
-//[]Once in click event, fire AJAX request to get DATA
-//[]Need to place that DATA in the DOM, (add to page)
+//[x]Hijack click event
+//[x]Once in click event, fire AJAX request to get DATA
+//[x]Need to place that DATA in the DOM, (add to page)
 $(function() {
   $('a.beer-styles-beer').on('click', function(e) {
     $.get(this.href).success(function(response) {
-      console.log(response)
+      $('div.beers').html(response)
     })
     e.preventDefault()
   })
