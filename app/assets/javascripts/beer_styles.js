c882b6f0cb7b54696f58b488cc234de4853a4e2b
Malind Kowalski <malind.nikulski@gmail.com>
2018-12-12 12:33:36 -0700

    able to log the response properly

--- app/assets/javascripts/beer_styles.js
+++ app/assets/javascripts/beer_styles.js
@@ -1,4 +1,12 @@
 //playing in .js to practice Rails + AJAX
-//[X]Hijack click event
-//[X]Once in click event, fire AJAX request to get DATA
-//[X]Need to place that DATA in the DOM, (add to page)
+//[]Hijack click event
+//[]Once in click event, fire AJAX request to get DATA
+//[]Need to place that DATA in the DOM, (add to page)
+$(function() {
+  $('a.beer-styles-beer').on('click', function(e) {
+    $.get(this.href).success(function(response) {
+      console.log(response)
+    })
+    e.preventDefault()
+  })
+})
